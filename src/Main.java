1663a0aab593534d4503221cd694f8cea9b090be
Domenico <domenicoribaudo06@gmail.com>
2024-01-11 00:31:14 +0100

    commit finale, esercizio completato

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
+//        Formatta la data ottenendo 01 marzo 2023
+//        Stampa sulla console
+
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        //Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
+        String inputString = "2023-03-01T13:00:00Z";
+        ZonedDateTime zonedDateTime = OffsetDateTime.parse(inputString).toZonedDateTime();
+
+        // Formatta la data ottenendo 01 marzo 2023 e Stampa sulla console
+        String dateFormatted = zonedDateTime.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
+        System.out.println(dateFormatted);
+
+    }
+}
